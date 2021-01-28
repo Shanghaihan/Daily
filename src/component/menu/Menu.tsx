2e9501472e97c8525a1062fbb743e77826aa935e
e <807196833@qq.com>
2021-01-29 00:59:21 +0800

    'add addModel'

--- src/component/menu/Menu.tsx
+++ src/component/menu/Menu.tsx
@@ -11,6 +11,7 @@ export const Menu:React.FC = ()=>{
     return(
         <div id = "menu" className = {styles.menu} >
             <Header/>
+            <Content/>
         </div>
     )
 }
@@ -57,7 +58,7 @@ const Header:React.FC = ()=>{
             origin= e.clientX - ref.current!.offsetLeft;
         }) 
         ref.current!.addEventListener('mouseup',function(e){
-            let distance:number = e.clientX - ref.current!.offsetLeft - origin;
+            let distance:number = e.clientX  - ref.current!.offsetLeft - origin;
             ref.current!.scrollLeft -= distance;
         });
         ref.current!.addEventListener('touchstart',function(e){
@@ -105,8 +106,9 @@ const Login:React.FC = ()=>{
     const [vis,setVis] = React.useState<boolean>(false);
     const [username,setUsername] = React.useState<string>('');
     const [password,setPassword] = React.useState<string>('');
-    const headRef = React.useRef<HTMLDivElement>(null);
+    const loginHeadRef = React.useRef<HTMLDivElement>(null);
     const HeadRef = React.useRef<HTMLDivElement>(null);
+    const outHeadRef = React.useRef<HTMLDivElement>(null);
     React.useEffect(()=>{
         const headerVis = (ref:React.RefObject<HTMLDivElement>,obj:string):void =>{
             if(data[obj] !== undefined){
@@ -120,7 +122,9 @@ const Login:React.FC = ()=>{
             }
         }
         headerVis(HeadRef,user);
-        headerVis(headRef,username);
+        headerVis(outHeadRef,user);
+        headerVis(loginHeadRef,username);
+
     },[username,user,data])
     return(
         <div>
@@ -130,32 +134,49 @@ const Login:React.FC = ()=>{
                     onClick = {()=>{setVis(true)}}
             >
             </div>
-            <MenuModel visible={vis} >
-                    <div 
-                        ref = {headRef}
-                        className  = {styles.loginHeader}
-                    >
-                    <div className = {styles.closeButton} onClick = {()=>{setVis(false)}}></div>
-                    </div>
-                    <input type="text" placeholder = 'username' onChange = {(e)=>{
-                                                                                setUsername(e.currentTarget.value);
-                                                                            }}
-                            value = {username}                                                
-                                                                            />
-                    <input type="password" placeholder = 'password' value = {password} onChange = {(e) =>{setPassword(e.currentTarget.value);}}/>
-                    <div className = {styles.loginButton} onClick = {()=>{
-                                                                        if(data[username] && data[username]['password'] === password){
-                                                                            window.sessionStorage.setItem('user',username);
-                                                                            setUser(username);
-                                                                            setVis(false);
-                                                                            setUsername('');
-                                                                            setPassword('');
-                                                                            var FileSaver = require('file-saver');
-                                                                            var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-                                                                            saveAs(blob, "hello world.txt");
-                                                                        }
-                                                                    }}
-                    >Login</div>
+            <MenuModel visible={vis && !user} >
+                        <div 
+                            ref = {loginHeadRef}
+                            className  = {styles.loginHeader}
+                        >
+                            <div className = {styles.closeButton} onClick = {()=>{setVis(false)}}></div>
+                        </div>
+                        <input type="text" placeholder = 'username' onChange = {(e)=>{
+                                                                                    setUsername(e.currentTarget.value);
+                                                                                }}
+                                value = {username}                                                
+                                                                                />
+                        <input type="password" placeholder = 'password' value = {password} onChange = {(e) =>{setPassword(e.currentTarget.value);}}/>
+                        <div className = {styles.loginButton} onClick = {()=>{
+                                                                            if(data[username] && data[username]['password'] === password){
+                                                                                window.sessionStorage.setItem('user',username);
+                                                                                setUser(username);
+                                                                                setVis(false);
+                                                                                setUsername('');
+                                                                                setPassword('');
+                                                                                // var FileSaver = require('file-saver');
+                                                                                // var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+                                                                                // saveAs(blob, "hello world.txt");
+                                                                            }
+                                                                        }}
+                        >Login</div>
+            </MenuModel>
+            <MenuModel visible = {vis && user}>
+                        <div 
+                            ref = {outHeadRef}
+                            className  = {styles.loginHeader}
+                        >
+                            <div className = {styles.closeButton} onClick = {()=>{setVis(false)}}></div>
+                        </div>
+                       <div style = {{marginBottom:'10px',color:'white'}}>{user}</div>
+                        <div className = {styles.loginButton} onClick = {()=>{
+                                                                                window.sessionStorage.setItem('user','');
+                                                                                setUser('');
+                                                                                setVis(false);
+                                                                        }}
+                            style = {{width:'100px'}}
+                        >Logout</div>
+
             </MenuModel>
         </div>
        
@@ -187,6 +208,75 @@ export const MenuModel:React.FC<modelType> = ({visible = false,children})=>{
     ,document.getElementById('menu') as Element)
 
 }
+
+
+const Content:React.FC = ()=>{
+    //@ts-ignore
+    const { day } = React.useContext(context);
+    const [vis,setVis] = React.useState<boolean>(false);
+    return(
+        <div className = {styles.Content}>
+            <div className = {styles.footer}>
+                <div className = {styles.addButton} onClick = {()=>{setVis(vis=>!vis)}}></div>
+            </div>
+            <AddModel visible = {vis}></AddModel>
+        </div>
+    )
+}
+
+
+export const AddModel:React.FC<modelType> = ({visible = false,children})=>{ 
+    const ref = React.useRef<HTMLDivElement>(null);
+    const [vis,setVis] = React.useState<boolean>(visible);
+    const [isClosing, setIsClosing] = React.useState(false);
+    const cnt = React.useRef(false);
+    React.useEffect(()=>{
+        if(isClosing){
+            const timerId = setTimeout(() => {setVis(false);},300);
+            return (): void => {
+              clearTimeout(timerId);
+              setIsClosing(false);
+            };
+        }else{
+             cnt.current === false ? cnt.current = true:setVis(true);
+        }
+    },[visible,isClosing]) 
+    if(!document.getElementById('menu')){
+        return null;
+    }
+    return ReactDOM.createPortal(
+        <div    className = {
+                                cn(
+                                    styles.menuModel,
+                                    {
+                                        [styles.modelVis]:vis
+                                    }                       
+                                )
+                }
+                ref = {ref}
+                onClick = {(e)=>{   if(e.currentTarget === e.target){
+                                        setIsClosing(true)}
+                                    }
+                                }
+            >
+            <div className = {
+                                cn(
+                                    styles.addContent,
+                                    {
+                                        [styles.slideIn]:!isClosing,
+                                        [styles.slideOut]:isClosing
+                                    }                       
+                                )
+                }>
+                <div style = {{width:'40px',height:'5px',background:'rgb(240, 238, 245)',margin:'0 auto',borderRadius:'8px'}}></div>
+                {children}                
+            </div>
+        </div> 
+        
+    ,document.getElementById('menu') as Element)
+
+}
+
 
 
 
